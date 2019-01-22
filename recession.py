68461776ff16c16244573ff79ee720eb70860cca
Daniel Waters <42608070+danielholderwaters@users.noreply.github.com>
2019-01-22 14:31:15 -0500

    Create recession.py

---
+++ recession.py
@@ -0,0 +1,17 @@
+#recession predictor
+#predict % likelihood of recession over next 8 quarters
+
+#get data: quarterly gdp data, leading economic indicators
+#all data can probably be found on FRED database
+
+#organize data (maybe recession is boolean, rather than GDP delta), turn all data quarterly
+
+#random forest model
+
+#train model
+
+#optimize on test data
+
+#predict % likelihood of recession over next 8 quarters
+
+#visualize forecast
